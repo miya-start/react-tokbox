8c9abcfcc8dacf56c86beaf55c5dc1e079b86a1c
miya-start <0000000to1@gmail.com>
2021-12-20 09:02:49 +0900

    feat: change background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const { sky, trueGray } = require('tailwindcss/colors')
+
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: ['./src/**/*.{js,jsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sky,
+        trueGray,
+        'trueGray-750': '#353535',
+        'trueGray-850': '#1f1f1f',
+      },
+    },
   },
   plugins: [],
 }
